e978deb523f5ec2033e6c6966a1150d2c8ba176c
carlitos321 <ca.cabrera321@gmail.com>
2017-12-12 21:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+function nondestructivelyUpdateObject(oldDriver, driverAdditions) {
+  return Object.assign({}, oldDriver, driverChanges);
+}
+
+const driver = {
+  name: 'Sam'
+};
+
+const newDriver = nondestructivelyUpdateObject(driver, 'address', '11 Broadway')
