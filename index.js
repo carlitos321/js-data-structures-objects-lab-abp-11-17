56e602766884b7395609f668f54513f780192888
carlitos321 <ca.cabrera321@gmail.com>
2017-12-12 21:36:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Write your solution in this file!
-let driver = {};
-
-function updateDriverWithKeyAndValue(driver, key, value) {
+let driver = {
 
 };
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return object.assign({}, driver, { [key]: value })
+};
+
+const newDriver = updateDriverWithKeyAndValue(driver, 'name', 'Sam');
 
 function nondestructivelyUpdateObject(oldDriver, driverAdditions) {
 
